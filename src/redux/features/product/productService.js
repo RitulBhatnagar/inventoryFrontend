010558419a4d88f4993b93c6ca618117dc8a349c
ritul bhatnagar <ritulbhatnagar375@gamil.com>
2023-03-14 20:34:01 +0530

    changesDone

--- src/redux/features/product/productService.js
+++ src/redux/features/product/productService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
-const API_URL = `https://inventory-4qa5.onrender.com/api/products/`;
+const API_URL = `${BACKEND_URL}/api/products/`;
 
 // Create New Product
 const createProduct = async (formData) => {
